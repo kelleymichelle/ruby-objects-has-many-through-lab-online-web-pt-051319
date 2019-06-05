83d6a5beb127dfff580a93d06bb1e0a3093f8e40
Kelley Chaplain <kmchaplain@gmail.com>
2019-06-05 02:44:43 -0500

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,8 +1,34 @@
 class Doctor
+  @@all = []
   attr_reader :name
 
   def initialize(name)
     @name = name
-  end  
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+  end
+  
+  def appointments
+    Appointment.all.select do |appt|
+      appt.doctor == self
+    end
+  end
+  
+  def patients
+    x = []
+    Appointment.all.each do |appt|
+      if appt.doctor == self
+        x << appt.patient
+      end  
+    end
+    return x
+  end    
 
 end  
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,8 +1,35 @@
+require 'pry'
 class Patient
+  @@all = []
   attr_reader :name
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+  
+  def appointments
+    Appointment.all.select do |appt|
+      appt.patient == self
+    end  
+  end
+  
+  def doctors
+    x = []
+    Appointment.all.each do |appt|
+      if appt.patient == self
+        x << appt.doctor
+      end
+    end
+    return x    
+  end  
   
 end  
